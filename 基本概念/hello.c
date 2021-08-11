2d2def9c883bf93c4c4bb0ae7a8c24e1dd155cb6
ares <709818574@qq.com>
2021-08-11 16:09:04 +0800

    优化

---
+++ 基本概念/hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+//	int *p = NULL;
+//	int i;
+
+//	p = (int *)malloc(sizeof(int));//内存释放	
+//	if(p=NULL)
+//		return -1;
+
+
+	printf("Hello world!\n");
+
+	return 0;		
+//	exit(0) //给内存看
+}
